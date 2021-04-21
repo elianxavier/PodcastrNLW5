ca51b299859479d0b2010334fbe5f6d4ef7cd2d4
Elian Xavier <45861861+elianxavier@users.noreply.github.com>
2021-04-21 09:34:16 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { GetStaticProps } from "next";
+import { api } from "..services/api";
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{ JSON.stringify(props.episodes) }</p>
+    </div>
+    
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes?_limit=12&_sort=published_at&_order=desc', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  return {
+    props: { episodes: data },
+    revalidate: 60 * 60 * 8,
+  }
+}
